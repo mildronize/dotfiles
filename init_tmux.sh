2fb91b6f63763adc46b3af1bc236581175114784
Thada Wangthammang <mildronize@gmail.com>
2017-02-11 23:32:41 +0700

    Reverse delete actions

---
+++ init_tmux.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Load configs
+source $PWD/configs.sh
+
+# Checking prerequisite
+check_prerequisite tmux
+check_prerequisite git
+check_prerequisite curl
+
+# Install Vim Plug ( Vim Plugin Manager )
+if [ ! -d $tmux_plugin_manager_path ]; then
+    git clone https://github.com/tmux-plugins/tpm $tmux_plugin_manager_path
+fi
